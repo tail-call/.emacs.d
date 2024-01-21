4332b4253e2a5a30e60c775c635c5fdb6fb22c60
Maria Zaitseva <maria@tail-call.ru>
2024-01-21 19:29:43 +0700

    Better space prefixed keys

--- init.el
+++ init.el
@@ -232,31 +232,35 @@ This is a vararg extension to `global-set-key'."
 
 ;; Evil mode SPC prefixed keys
 
-(defun define-spc-command (key function)
-  (define-key evil-normal-state-map (kbd (format "SPC %s" key)) function))
+(cl-labels ((define-spc-command (key function)
+              (define-key evil-normal-state-map
+                          (kbd (format "SPC %s" key)) function)))
+  (define-spc-command "a" 'ag)
+  (define-spc-command "w" 'widen)
+  (define-spc-command "c" 'quick-calc)
+  (define-spc-command "g" 'magit-status)
 
-(define-spc-command "a" 'ag)
-(define-spc-command "w" 'widen)
-(define-spc-command "c" 'quick-calc)
-(define-spc-command "g" 'magit-status)
+  (define-spc-command
+   "o m"
+   (lambda () (interactive)
+     (find-file "~/Library/Mobile Documents/com~apple~CloudDocs/org/INDEX.org")))
 
-(define-spc-command "o m"
-  (lambda () (interactive)
-    (find-file "~/Library/Mobile Documents/com~apple~CloudDocs/org/INDEX.org")))
+  (define-spc-command
+   "i"
+   (lambda () (interactive)
+     (setq-default indent-tabs-mode t)
+     (setq-default tab-width 4)))
 
-(define-spc-command "i"
-  (lambda () (interactive)
-    (setq-default indent-tabs-mode t)
-    (setq-default tab-width 4)))
+  (define-spc-command
+   "I"
+   (lambda () (interactive)
+     (setq-default indent-tabs-mode nil)
+     (setq-default tab-width 8)))
 
-(define-spc-command "I"
-  (lambda () (interactive)
-    (setq-default indent-tabs-mode nil)
-    (setq-default tab-width 8)))
-
-(define-spc-command "w"
-  (lambda () (interactive)
-    (setq truncate-lines (not truncate-lines))))
+  (define-spc-command
+   "w"
+   (lambda () (interactive)
+     (setq truncate-lines (not truncate-lines)))))
 
 ;; (define-key evil-normal-state-map (kbd "SPC b c") 'erase-buffer)
 ;; (define-key evil-normal-state-map (kbd "SPC b n") 'switch-to-next-buffer)
